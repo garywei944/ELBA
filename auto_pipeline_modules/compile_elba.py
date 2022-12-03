0a73c4a27cc416ca34bb70c55885aaca1db9bffc
Gabriel Raulet <gabe.h.raulet@gmail.com>
2022-12-02 22:44:42 -0800

    works on mac

--- auto_pipeline_modules/compile_elba.py
+++ auto_pipeline_modules/compile_elba.py
@@ -9,30 +9,29 @@ def logmsg(msg):
     sys.stderr.write(msg + "\n")
     sys.stderr.flush()
 
-def abolsolutize(p):
-    return p.absolute().resolve()
-
 def usage():
     sys.stderr.write("Usage: python {} [options] /path/to/progdir\n\n".format(sys.argv[0]))
     sys.stderr.write("Options:\n")
     sys.stderr.write("    -L INT,INT,...   prune k-mers below this bound [20]\n")
     sys.stderr.write("    -U INT,INT,...   prune k-mers above this bound [30]\n")
-    sys.stderr.write("    -d FLOAT chernoff bound [0.1]\n")
-    sys.stderr.write("    -p FILE  relative path to ELBA main directory [../]\n")
+    sys.stderr.write("    -d FLOAT         chernoff bound [0.1]\n")
+    sys.stderr.write("    -p PATH          ELBA main directory [../]\n")
+    sys.stderr.write("    -I PATH          compilation directory [compilation]\n")
     return -1
 
 def main(argc, argv):
 
     if argc < 2: return usage()
 
-    elba_path = Path("..").absolute().resolve()
+    source_path = Path("..").resolve()
+    compilation_path = Path("compilation").resolve()
     lower_kmer_bounds = [20]
     upper_kmer_bounds = [30]
     delta_chernoff = 0.1
 
-    try: opts, args = getopt.gnu_getopt(argv[1:], "L:U:d:p:h")
+    try: opts, args = getopt.gnu_getopt(argv[1:], "L:U:d:p:I:h")
     except getopt.GetoptError as err:
-        sys.stderr.write("error: {}\n".format(err))
+        msglog("error: {}".format(err))
         return usage()
 
     for o, a in opts:
@@ -40,28 +39,63 @@ def main(argc, argv):
         elif o == "-L": lower_kmer_bounds = list(map(lambda x: int(x), a.split(",")))
         elif o == "-U": upper_kmer_bounds = list(map(lambda x: int(x), a.split(",")))
         elif o == "-d": delta_chernoff = float(a)
+        elif o == "-p": source_path = Path(a).resolve()
+        elif o == "-I": compilation_path = Path(a).resolve()
 
     if len(args) != 1:
         return usage()
 
-    for l in L:
-        for u in U:
-            if l >= u:
-                sys.stderr.write("error: can't have a lower k-mer bound greater than an upper k-mer bound\n")
-                return usage()
+    progdir_path = Path(args[0]).resolve()
+    progdir_path.mkdir(exist_ok=True)
 
-    #outdir_path = absolutize(Path(args[0]))
-    outdir_path = Path(args[0])
-    outdir_path.mkdir(parents=True, exist_ok=True)
+    elba_build_path = compilation_path.joinpath("elba_build")
+    elba_install_path = compilation_path.joinpath("elba_install")
+    combblas_build_path = compilation_path.joinpath("combblas_build")
+    combblas_install_path = compilation_path.joinpath("combblas_install")
 
-    logmsg("Created/touched output directory " + str(outdir_path))
+    elba_build_path.mkdir(parents=True, exist_ok=True)
+    elba_install_path.mkdir(parents=True, exist_ok=True)
+    combblas_build_path.mkdir(parents=True, exist_ok=True)
+    combblas_install_path.mkdir(parents=True, exist_ok=True)
 
-    for p in outdir_path.iterdir():
-        if p.is_dir():
-            shutil.rmtree(str(p))
-            logmsg("Removed directory '{}'".format(str(p)))
-        elif p.is_file():
-            p.unlink()
-            logmsg("Removed file '{}'".format(str(p)))
+    combblas_path = source_path.joinpath("CombBLAS")
 
-    build_path = outdir_path.joinpath(
+    if not combblas_path.is_dir():
+        logmsg("error: CombBLAS not found at '{}'".format(str(combblas_path)))
+
+    combblas_build_setup_cmd = ["cmake", "-S", str(combblas_path), "-B", str(combblas_build_path), "-DCMAKE_INSTALL_PREFIX=" + str(combblas_install_path), "-DCMAKE_C_COMPILER=gcc-11", "-DCMAKE_CXX_COMPILER=g++-11"]
+    logmsg(" ".join(combblas_build_setup_cmd))
+    proc = sp.Popen(combblas_build_setup_cmd)
+    proc.wait()
+
+    combblas_build_cmd = ["make", "-j", str(12), "-C", str(combblas_build_path)]
+    logmsg(" ".join(combblas_build_cmd))
+    proc = sp.Popen(combblas_build_cmd)
+    proc.wait()
+
+    combblas_install_cmd = ["make", "install", "-j", str(12), "-C", str(combblas_install_path)]
+    logmsg(" ".join(combblas_install_cmd))
+    proc = sp.Popen(combblas_build_cmd)
+    proc.wait()
+
+    for l in lower_kmer_bounds:
+        for u in upper_kmer_bounds:
+            elba_build_setup_cmd = ["cmake", "-S", str(source_path), "-B", str(elba_build_path),
+                                    "-DCMAKE_C_COMPILER=gcc-11", "-DCMAKE_CXX_COMPILER=g++-11",
+                                    "-DLOWER_KMER_FREQ="+str(l), "-DUPPER_KMER_FREQ="+str(u),
+                                    "-DDELTACHERNOFF="+str(delta_chernoff)]
+            logmsg(" ".join(elba_build_setup_cmd))
+            proc = sp.Popen(elba_build_setup_cmd)
+            proc.wait()
+
+            elba_build_cmd = ["make", "-j", str(12), "-C", str(elba_build_path)]
+            logmsg(" ".join(elba_build_cmd))
+            proc = sp.Popen(elba_build_cmd)
+            proc.wait()
+
+            shutil.copy(str(elba_build_path.joinpath("elba")), str(progdir_path.joinpath("elba.l{}u{}".format(l, u))))
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main(len(sys.argv), sys.argv))
