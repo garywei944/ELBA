16048ba43fdf00f92150e8687cfbff4e783c50d5
Gabriel Raulet <gabe.h.raulet@gmail.com>
2022-12-05 13:45:36 -0800

    auto scripts go in PATH for headache reduction

--- saul-env.sh
+++ saul-env.sh
@@ -4,9 +4,12 @@ module load PrgEnv-gnu
 module load python
 
 export COMBBLAS_HOME=$PWD
+export ELBA_HOME=$PWD
 export BLOOM_HOME=$PWD/src/libbloom/
 export SEQAN_HOME=$PWD/seqan
 
 export OMP_NUM_THREADS=1
 export OMP_PROC_BIND=spread
 export OMP_PLACES=threads
+
+export PATH=$PATH:$ELBA_HOME/auto_pipeline_modules
