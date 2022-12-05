704b7e748031a84f07b7033cc50478b23dbfaaa7
Gabriel Raulet <gabe.h.raulet@gmail.com>
2022-12-05 14:12:21 -0800

    parse quast table

--- auto_pipeline_modules/process_elba.py
+++ auto_pipeline_modules/process_elba.py
@@ -43,7 +43,6 @@ def main(argc, argv):
     for logfile in outdir_path.glob("elba_rank_*.txt"):
         logfile.unlink()
 
-
     if reference_fname != "":
         reference_path = Path(reference_fname).resolve()
         if not reference_path.is_file():
@@ -66,8 +65,55 @@ def main(argc, argv):
 
         quast_cmd = ["quast.py", "-o", str(quast_outdir), "-r", str(reference_path), str(contigs_path)]
         logmsg("executing: '{}'\n".format(" ".join(quast_cmd)))
-        proc = sp.Popen(quast_cmd)
+        proc = sp.Popen(quast_cmd, stdout=sp.stderr)
         proc.wait()
+
+        gen_report_path = quast_outdir.joinpath("report.tsv").resolve()
+
+        if not gen_report_path.is_file():
+            logmsg("error: could not find '{}'".format(str(gen_report_path)))
+            return -1
+
+        quast_info_dict = {}
+        quast_info_dict["contig_file"] = str(contigs_path)
+
+        for line in open(str(gen_report_path), "r"):
+            if line.startswith("# contigs"):
+                quast_info_dict["num_contigs"] = int(line.rstrip().split("\t")[1])
+            elif line.startswith("Largest contig"):
+                quast_info_dict["largest_contig"] = int(line.rstrip().split("\t")[1])
+            elif line.startswith("Total length"):
+                quast_info_dict["total_length"] = int(line.rstrip().split("\t")[1])
+            elif line.startswith("Reference_length"):
+                quast_info_dict["ref_length"] = int(line.rstrip().split("\t")[1])
+            elif line.startswith("N50"):
+                quast_info_dict["N50"] = int(line.rstrip().split("\t")[1])
+            elif line.startswith("NG50"):
+                quast_info_dict["NG50"] = int(line.rstrip().split("\t")[1])
+            elif line.startswith("N90"):
+                quast_info_dict["N90"] = int(line.rstrip().split("\t")[1])
+            elif line.startswith("NG90"):
+                quast_info_dict["NG90"] = int(line.rstrip().split("\t")[1])
+            elif line.startswith("# misassemblies"):
+                quast_info_dict["num_misassemblies"] = int(line.rstrip().split("\t")[1])
+            elif line.startswith("# misassembled contigs"):
+                quast_info_dict["num_misassembled_contigs"] = int(line.rstrip().split("\t")[1])
+            elif line.startswith("Genome fraction"):
+                quast_info_dict["completeness_fraction"] = float(line.rstrip().split("\t")[1])/100
+            elif line.startswith("# mismatches per 100 kbp"):
+                quast_info_dict["mismatches_per_100_kbp"] = float(line.rstrip().split("\t")[1])
+            elif line.startswith("# indels per 100 kbp"):
+                quast_info_dict["indels_per_100_kbp"] = float(line.rstrip().split("\t")[1])
+            elif line.startswith("Duplication ratio"):
+                quast_info_dict["duplication_ratio"] = float(line.rstrip().split("\t")[1])
+            elif line.startswith("Largest alignment"):
+                quast_info_dict["largest_alignment"] = int(line.rstrip().split("\t")[1])
+            elif line.startswith("Total aligned length"):
+                quast_info_dict["total_aligned_length"] = int(line.rstrip().split("\t")[1])
+
+        for key, value in quast_info_dict.items():
+            sys.stdout.write("{}\t{}\n".format(key, str(value)))
+        sys.stdout.flush()
 
 
 if __name__ == "__main__":
--- mac-env.sh
+++ mac-env.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 export COMBBLAS_HOME=$PWD
+export ELBA_HOME=$PWD
 export BLOOM_HOME=$PWD/src/libbloom/
 export SEQAN_HOME=$PWD/seqan
 export OMP_NUM_THREADS=1
+export PATH=$PATH:$ELBA_HOME/auto_pipeline_modules
